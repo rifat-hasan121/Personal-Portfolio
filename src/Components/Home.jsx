a79016f4ae305c875bb7c1399a83602185ac5f60
Rifat Hasan <rifathasannetrakona@gmail.com>
2025-05-30 22:34:39 +0600

    added home page img link

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -55,10 +55,7 @@ const Home = () => {
           {/* Image Section */}
           <div className="relative w-[594px] h-[535px] md:w-[794px] md:h-[735px] overflow-hidden">
             <div className="absolute inset-0 transform -rotate-6 clip-custom">
-              <div
-                className="w-full h-full bg-[url('../../src/assets/riat-removebg-preview-EDIT-Picsart-AiImageEnhancer.jpg')] bg-no-repeat bg-center bg-cover"
-               
-              ></div>
+              <div className="w-full h-full bg-[url('https://i.ibb.co/8gF60LMm/android-chrome-512x512.png')] bg-no-repeat bg-center bg-cover"></div>
             </div>
           </div>
 
